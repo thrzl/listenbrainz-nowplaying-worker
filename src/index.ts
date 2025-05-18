7f08ad38dd634edae8b837753b17205f249b9a0e
te <thrzl@icloud.com>
2025-05-18 11:12:44 -0400

    allow access control header

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,9 @@ export default {
 		if (!user) {
 			return Response.json(
 				{ error: "no user param specified" },
-				{ status: 400, statusText: "no user param specified" },
+				{ status: 400, statusText: "no user param specified", headers: {
+					"Access-Control-Allow-Origin": "*"
+				} },
 			);
 		}
 
